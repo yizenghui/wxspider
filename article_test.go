064bdf8e33a3ba394c1450e14d27ef89e98fe57e
yizenghui <245561237@qq.com>
2018-09-07 10:18:28 +0800

    s

--- article_test.go
+++ article_test.go
@@ -20,8 +20,9 @@ func Test_PostArticle(t *testing.T) {
 func Test_PostArticleOne(t *testing.T) {
 	rows := GetArticles()
 	for _, row := range rows {
-		e := PostArticle(row)
-		t.Fatal(e)
+		// e := PostArticle(row)
+		t.Fatal(row)
+		// t.Fatal(e)
 	}
 	t.Fatal(rows)
 }
