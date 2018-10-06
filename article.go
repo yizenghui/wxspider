416e79003c69ba5acc45d9ec791556946541af27
yizenghui <245561237@qq.com>
2018-10-06 09:44:03 +0800

    wx.readfollow.com 域名不再服务了

--- article.go
+++ article.go
@@ -192,7 +192,7 @@ func PostArticle(article Article) (int64, error) {
 
 	// resp, err := client.PostForm("http://wxapi.readfollow.com/api/v1/article", data)
 	// resp, err := client.PostForm("https://wechatrank.com/api/links/", data)
-	resp, err := client.PostForm("https://wx.readfollow.com/api/links/", data)
+	resp, err := client.PostForm("https://wechatrank.com/api/links/", data)
 	// resp, err := client.PostForm("http://wxapi.oo/api/links/", data)
 	// resp, err := client.PostForm("http://wxapi.cc:626/api/links/", data)
 	// resp.Body.Close()
